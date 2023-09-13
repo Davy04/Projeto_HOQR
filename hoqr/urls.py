6eabd59f144e3dcf5181c671e9ed3929de3d4f39
Lucas Nobre <lucasnobreesteves@gmail.com>
2023-09-13 14:35:43 -0300

    troca url pacientes

--- hoqr/urls.py
+++ hoqr/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('paciente/', include('pacientes.urls'))
+    path('pacientes/', include('pacientes.urls'))
 ]
